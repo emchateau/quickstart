77a19b738a3aabcbeaa1949633574f8d6d225d41
Emmanuel Chateau <emchateau@laposte.net>
2016-07-05 12:37:04 -0400

    tutorial heroes part2, ajout styles + sélection

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,8 +9,9 @@ export class Hero {
 }
 
 /* @Component décoration
-   avec des liens vers les propriétés définies dans le composant d’application
-   utilisation d’une directive prédéfinie ngModel
+   directive pour itérer sur les valeurs de heroes
+   ajout d’un événement au click
+   ajout d’une directive ngIf
 */
 @Component({
   selector: 'my-app',
@@ -19,12 +20,69 @@ export class Hero {
     <div>
       <h2>My heroes</h2>
       <ul class="heroes">
-        <li *ngFor="let hero of heroes">
+        <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
           <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
       </ul>
+      <div *ngIf="selectedHero">
+        <h2>{{selectedHero.name}} details!</h2>
+        <div><label>id:   </label>{{selectedHero.id}}</div>
+        <div>
+          <label>name: </label>
+          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+        </div>
+      </div>
     </div>
-  `
+  `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 
 /* Composant d’application
@@ -33,6 +91,8 @@ export class Hero {
 export class AppComponent {
   title = 'tour of Heroes';
   public heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
 /* Array créant des objets de la classe Hero
