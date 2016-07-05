3132b1e91857ad4c195ccee7b94ca6492c92a223
Emmanuel Chateau <emchateau@laposte.net>
2016-07-05 12:09:41 -0400

    tutorial heroes part2, iterate on heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,14 +16,13 @@ export class Hero {
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details !</h2>
     <div>
-      <label>id: </label>
-      <input value="{{hero.id}}" placeholder="name">
-    </div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+      <h2>My heroes</h2>
+      <ul class="heroes">
+        <li *ngFor="let hero of heroes">
+          <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+      </ul>
     </div>
   `
 })
