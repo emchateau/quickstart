892081d09527c7765064b20e94c01e441e098c63
Emmanuel Chateau <emchateau@laposte.net>
2016-07-05 09:57:08 -0400

    tutoriel heros part1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,39 @@
 import { Component } from '@angular/core';
 
+/* Classe Hero
+   création d’une classe
+*/
+export class Hero {
+  id: number;
+  name: string;
+}
+
+/* @Component décoration
+   avec des liens vers les propriétés définies dans le composant d’application
+*/
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details !</h2>
+    <div>
+      <label>id: </label>
+      <input value="{{hero.id}}" placeholder="name">
+    </div>
+    <div>
+      <label>name: </label>
+      <input value="{{hero.name}}" placeholder="name">
+    </div>
+  `
 })
-export class AppComponent { }
+
+/* Composant d’application
+   avec deux propriétés
+*/
+export class AppComponent {
+  title = 'tour of Heroes';
+  hero : Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
